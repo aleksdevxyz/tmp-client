fa219f19b0b972ef1a835fd1b770555bb2581a51
serega240805 <ssuchalkin@yandex.ru>
2024-06-07 22:27:42 +0300

    revert: отменены изменения на главной странице из-за плохой оптимизации

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,20 +1,17 @@
-import dynamic from 'next/dynamic';
-import { Metadata } from 'next';
-import { getTranslations } from 'next-intl/server';
-import styles from './page.module.scss';
-
-const ChannelsList = dynamic(() => import('@/components/ChannelsList/ChannelsList'));
-const MainSection = dynamic(() => import('@/components/MainSection/MainSection'));
-const NewChannels = dynamic(() => import('@/components/NewChannels/NewChannels'));
-const Pagination = dynamic(() => import('@/components/Pagination/Pagination'));
-const RecList = dynamic(() => import('@/components/RecListMain/RecListMain'));
-
+import ChannelsList from "@/components/ChannelsList/ChannelsList";
+import MainSection from "@/components/MainSection/MainSection";
+import NewChannels from "@/components/NewChannels/NewChannels";
+import Pagination from "@/components/Pagination/Pagination";
+import RecList from "@/components/RecListMain/RecListMain";
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+import styles from "./page.module.scss";
 export async function generateMetadata(): Promise<Metadata> {
-  const t = await getTranslations('Index');
+  const t = await getTranslations("Index");
   return {
-    title: t('title'),
-    description: t('description'),
-    keywords: t('keywords'),
+    title: t("title"),
+    description: t("description"),
+    keywords: t("keywords"),
     robots: {
       index: true,
       follow: true,
@@ -32,30 +29,38 @@ export interface ChannelsProps {
 }
 
 async function getChannelsList(page: number) {
-  const res = await fetch(`${process.env.BASE_URL}/channels?page=${page}&limit=31`);
+  const res = await fetch(
+    `${process.env.BASE_URL}/channels?page=${page}&limit=31`
+  );
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
   return res.json();
 }
 
-export default async function Home({ searchParams }: { searchParams?: { page?: number; totalPages?: number; }; }) {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams?: {
+    page?: number;
+    totalPages?: number;
+  };
+}) {
   const currentPage = Number(searchParams?.page) || 0;
 
-  const [ChannelsData, t] = await Promise.all([
-    getChannelsList(currentPage),
-    getTranslations('Main'),
-  ]);
+  const ChannelsData = await getChannelsList(currentPage);
+
+  const t = await getTranslations("Main");
 
   return (
     <main>
       <MainSection />
       <div className={styles.section}>
-        <h2 className={styles.title}>{t('Телеграм каналы')}</h2>
+        <h2 className={styles.title}>{t("Телеграм каналы")}</h2>
         <ChannelsList advertisement={true} data={ChannelsData} />
       </div>
       <div className={styles.section}>
-        <h2 className={styles.title}>{t('Новые каналы')}</h2>
+        <h2 className={styles.title}>{t("Новые каналы")}</h2>
         <NewChannels />
       </div>
       <div className={styles.counter}>
