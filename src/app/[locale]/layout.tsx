35b37974c87124c19d04e05e09d0a9592a7d0526
serega240805 <ssuchalkin@yandex.ru>
2024-07-22 12:17:36 +0300

    fix(styles): исправил косяки и баги с весткой

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -38,6 +38,7 @@ export default async function RootLayout({
     <html lang={locale}>
       <body suppressHydrationWarning={true} className={inter.className}>
         <main
+          id={"main"}
           style={{
             maxWidth: "100vw",
             display: "flex",
@@ -45,7 +46,6 @@ export default async function RootLayout({
             minHeight: "100vh",
             position: "relative",
             backgroundColor: "rgb(250, 251, 254)",
-            paddingBottom: "42px",
           }}
         >
           <NextIntlClientProvider locale={locale} messages={messages}>
