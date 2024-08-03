63661fe3fb18d1fc7c219159c770de5af2fbde15
serega240805 <ssuchalkin@yandex.ru>
2024-08-03 18:19:16 +0300

    fix: исправил баг с хлебными крошками

--- src/components/BreadCrumbs/BreadCrumbs.tsx
+++ src/components/BreadCrumbs/BreadCrumbs.tsx
@@ -4,15 +4,19 @@ import { useLocale, useTranslations } from "next-intl";
 import styles from "./BreadCrumbs.module.scss";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+
 export default function BreadCrumbs({ name, category }: { name?: any, category?: string }) {
   const pathName = usePathname();
   const locale = useLocale();
   const currentPath = pathName
     .split("/")
     .slice(2, -1)
-    .map((item) => item.replace("channel", "Каналы"));
+    .map((item) => {
+      if (item === "channel") return "Каналы";
+      if (item === "articles") return "Статьи";
+      return item;
+    });
   const t = useTranslations("Breadcrumb");
-
 
   return (
     <ul className={styles.breadCrumbs}>
@@ -24,22 +28,19 @@ export default function BreadCrumbs({ name, category }: { name?: any, category?:
       {pathName === `/${locale}/articles` ? (
         <li className={styles.breadCrumbs_item_last}>{t("Статьи")}</li>
       ) : (
-        currentPath.map((item, index) => {
-          return (
-            <li className={styles.breadCrumbs_item} key={index}>
-              <Link
-                className={styles.breadCrumbs_item}
-                href={`/${locale}/${item}`}
-              >
-                {item === "bots"
-                  ? t("Боты")
-                  : item === "chats"
-                  ? t("Чаты")
-                  : t("Статьи")}
-              </Link>
-            </li>
-          );
-        })
+        currentPath.map((item, index) => (
+          <li className={styles.breadCrumbs_item} key={index}>
+            <Link className={styles.breadCrumbs_item} href={`/${locale}/${item}`}>
+              {item === "Каналы"
+                ? t("Каналы")
+                : item === "bots"
+                ? t("Боты")
+                : item === "chats"
+                ? t("Чаты")
+                : t("Статьи")}
+            </Link>
+          </li>
+        ))
       )}
       {category && <li className={styles.breadCrumbs_item_last}>{category}</li>}
       {name && <li className={styles.breadCrumbs_item_last}>{name}</li>}
